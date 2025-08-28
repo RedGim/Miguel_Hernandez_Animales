4ff2231d6b89007e32d1a96c02bcef728f168c43
redgim <miguelhdz3631@gmail.com>
2025-08-28 09:04:34 -0500

    Clases

--- AppCon/Program.cs
+++ AppCon/Program.cs
@@ -1,1 +1,36 @@
 ﻿Console.WriteLine("Hello, World!");
+
+public class Especies{
+    public int id;
+    public string? nombre;
+    public bool extinto;
+    public List<Animales>? animales;
+    
+}
+
+public class Ubicacion{
+    public int id;
+    public string? ciudad;
+    public string? pais;
+    public List<Animales>? lnimales;
+    public List<Lugares>? lugares;
+
+}
+
+public class Animales {
+    public int id;
+    public string? nombre_vulgar;
+    public int edad;
+    public DateTime fecha_nacimiento;
+   
+}
+
+public class Lugares {
+    public int id;
+    public string? nombre;
+    public decimal presupuesto;
+    public int numero_animales;
+    public List<Animales>? animales;
+
+}
+
